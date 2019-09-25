e648e06fecd0ba857a093a76f9fb911eb0fcb041
bwen1 <bwen1@live.com>
2019-09-25 15:23:20 +1000

    Added a Bill Page

---
+++ TimetableTalkShell/TimetableTalkShell/TimetableTalkShell/ViewModels/BillViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TimetableTalkShell.ViewModels
+{
+    public class BillViewModel : ContentPage
+    {
+        public BillViewModel()
+        {
+            Title = "Bill";
+        }
+    }
+}
