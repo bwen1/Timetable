17836e28936c2000383f67f8eeca6f77f1c30090
Hano555 <Hanogy@gmail.com>
2019-11-03 12:45:26 +1000

    Created a project for unit testing to be done. Also first unit test method is half done. We just need to fix our navigation and we are good to go

---
+++ TimetableTalkShell/TimetableTest/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimetableTalkShell.ViewModels;
+
+
+namespace TimetableTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "hanogy@gmail.com";
+            vm.Password = "hobolobo456";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "", "Email is Invalid");// Wrong Email
+            Assert.IsTrue(vm.Password == "", "Password is Invalid");// Wrong Password
+        }
+    }
+}
