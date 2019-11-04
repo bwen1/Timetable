e14a0f893492970f6c6be812e389679efdea8a69
Hano555 <Hanogy@gmail.com>
2019-11-04 12:32:24 +1000

    Added more tests within the two pages i was working for unexpected and expected results.

--- TimetableTalkShell/TimetableTest/UnitTests.cs
+++ TimetableTalkShell/TimetableTest/UnitTests.cs
@@ -7,7 +7,7 @@ namespace TimetableTest
     public class SignUpViewModelTest
     {
         [TestMethod]
-        public void SignupCommandTest()
+        public void SignupCommandTestwithemailandpassword()
         {
             //Arrange
             var vm = new SignUpViewModel();
@@ -19,13 +19,118 @@ namespace TimetableTest
             vm.SignUpCommand.Execute(null);
 
             //Assert
-            Assert.IsTrue(vm.GetType().Name == "SignUpViewModel", "viewmodel");// Correct result
-            //Assert.IsTrue(vm.Email == "", "Email is Invalid");// Wrong result
-            //Assert.IsTrue(vm.Password == "", "Password is Invalid");// Wrong result
+            Assert.IsTrue(vm.Email == "hanogy@gmail.com", "Valid Email");//Correct result
+            Assert.IsTrue(vm.Password == "hobolobo456", "Valid Password");//Correct result
+            
         }
 
 
+        [TestMethod]
+        public void SignupCommandTestwithnoemailandpassword()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "";
+            vm.Password = "";
 
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "", "Invalid Email");//Wrong result
+            Assert.IsTrue(vm.Password == "", "Invalid Password");//Wrong result
+
+        }
+
+        [TestMethod]
+        public void SignupCommandTestwithemailandnopassword()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "hanogy@gmail.com";
+            vm.Password = "";
+
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "hanogy@gmail.com", "Valid Email");//Correct result
+            Assert.IsTrue(vm.Password == "", "Invalid Password");//Wrong result
+
+        }
+
+        [TestMethod]
+        public void SignupCommandTestwithpasswordandnoemail()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "";
+            vm.Password = "Xlriggde";
+
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "", "Invalid Email");//Wrong result
+            Assert.IsTrue(vm.Password == "Xlriggde", "Valid Password");//Correct result
+
+        }
+
+        public void SignupCommandTestwithincorrectemailandpassword()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "alexmercer.com";
+            vm.Password = "Xlriggde";
+
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "alexmercer.com", "Invalid Email");//Wrong result
+            Assert.IsTrue(vm.Password == "Xlriggde", "Valid Password");//Correct result
+
+        }
+
+
+        public void SignupCommandTestwithspecialcharemailandpassword()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "-/[][;[@gmail.com";
+            vm.Password = ":():";
+
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "-/[][;[@gmail.com", "Invalid Email");//Wrong result
+            Assert.IsTrue(vm.Password == ":():", "Invalid Password");//Wrong result
+
+        }
+
+
+        public void SignupCommandTestwithalreadytakenemailandnewpassword()
+        {
+            //Arrange
+            var vm = new SignUpViewModel();
+            vm.Email = "billwen23@gmail.com";//existing email
+            vm.Password = "snogeojoe";
+
+
+            //Act
+            vm.SignUpCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "billwen23@gmail.com", "Email taken! Try a new one");//Wrong result
+            Assert.IsTrue(vm.Password == "snogeojoe", "Valid Password");//Correct result
+
+        }
     }
 
     [TestClass]
@@ -46,8 +151,96 @@ namespace TimetableTest
 
             //Assert
             Assert.IsTrue(vm.GetType().Name == "LoginViewModel", "viewmodel");// Correct result
-            //Assert.IsTrue(vm.Name == "", "Name is Invalid");// Wrong result
-            //Assert.IsTrue(vm.Password == "", "Password is Invalid");// Wrong result
+            
+        }
+
+
+        [TestMethod]
+        public void LoginClickedCommandTestwithnousernameandpassword()
+        {
+            //Arrange
+            var vm = new LoginViewModel();
+            vm.Name = "";
+            vm.Password = "";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Name == "", "Invalid Username");//Wrong result
+            Assert.IsTrue(vm.Password == "", "Invalid Password");//Wrong result
+
+        }
+
+        [TestMethod]
+        public void LoginClickedCommandTestwithusernameandnopassword()
+        {
+            //Arrange
+            var vm = new LoginViewModel();
+            vm.Name = "Shampoo345";
+            vm.Password = "";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Name == "Shampoo345", "Valid Username");//Correct result
+            Assert.IsTrue(vm.Password == "", "Invalid Password");//Wrong result
+
+        }
+
+        [TestMethod]
+        public void LoginClickedCommandTestwithpasswordandnousername()
+        {
+            //Arrange
+            var vm = new LoginViewModel();
+            vm.Name = "";
+            vm.Password = "Xlriggde";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Name == "", "Invalid username");//Wrong result
+            Assert.IsTrue(vm.Password == "Xlriggde", "Valid Password");//Correct result
+
+        }
+
+        public void LoginClickedCommandTestwithspecialcharusernameandpassword()
+        {
+            //Arrange
+            var vm = new LoginViewModel();
+            vm.Name = "-/[][;[??.";
+            vm.Password = "}:():}";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Email == "-/[][;[??.", "Invalid Username");//Wrong result
+            Assert.IsTrue(vm.Password == "}:():}", "Invalid Password");//Wrong result
+
+        }
+
+        public void LoginCommandTestwithalreadytakenusernameandnewpassword()
+        {
+            //Arrange
+            var vm = new LoginViewModel();
+            vm.Name = "JacobFreeman34";//existing username
+            vm.Password = "snogeojoe";
+
+
+            //Act
+            vm.LoginCommand.Execute(null);
+
+            //Assert
+            Assert.IsTrue(vm.Name == "JacobFreeman34", "Username taken! Try a new one");//Wrong result
+            Assert.IsTrue(vm.Password == "snogeojoe", "Valid Password");//Correct result
+
         }
 
     }
